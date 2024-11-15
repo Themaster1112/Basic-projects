33f36d02f09a1282ef8dd65c5143667af6e0622d
Pythonforbasics <114282587+Themaster1112@users.noreply.github.com>
2024-11-15 17:31:06 +0000

    New project

---
+++ Ink percentage counter/Main code.py
@@ -0,0 +1,5 @@
+Whydoyouneedthis=input("How much ink do you have respond in: Less than 25 or More than 25 percent")
+if Whydoyouneedthis == "Less than 25 percent":
+    print("Buy some ink")
+else:
+    print("Your fine :D")
